ce16a4f66376e523b0a77d119601d9c610a966e9
David Pellecuer <pellecuer.david@gmail.com>
2018-04-05 21:24:25 +0200

    Formulaire entrée

---
+++ src/post_verif.php
@@ -0,0 +1,41 @@
+<?php
+
+
+$message = $_POST['message'];
+$errors =[];
+
+if (!array_key_exists('customRadio1', $_POST) || $_POST ['customRadio1'] == ""){
+    $errors['customRadio1'] = "Vous n'avez pas choisi votre personnage";
+}
+
+
+if (!array_key_exists('customRadio2', $_POST) || $_POST ['customRadio2'] == ""){
+    $errors['customRadio2'] = "Vous n'avez pas choisi votre adversaire";
+}
+
+if ($_POST ['customRadio1'] == $_POST ['customRadio2']){
+    $errors['customRadio3'] = "Vous devez choisir deux personnages différents";
+}
+
+session_start();
+
+if(!empty($errors)) {
+
+    $_SESSION['errors'] = $errors;
+    $_SESSION['inputs'] =$_POST;
+    var_dump($_POST);
+    header('location:Form.php');
+
+
+
+
+} else {
+    $_SESSION['success'] = 1;
+    $_SESSION['inputs'] =$_POST;
+    header('location:Form.php');
+    $message =$_POST['message'];
+
+
+    }
+
+
