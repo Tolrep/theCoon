6e967614fe07549b22fa92571e5e835e80f69f2b
David Pellecuer <pellecuer.david@gmail.com>
2018-04-06 13:55:22 +0200

    dernier commit

---
+++ src/progressBar.php
@@ -0,0 +1,6 @@
+<div class="progress">
+    <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar"
+         aria-valuenow="<?=$hero[$idAd]->force?>" aria-valuemin="0" aria-valuemax="100" style="width:<?=$hero[$idAd]->force?>%">
+        <?=$hero[$idAd]->force?>
+    </div>
+</div>
