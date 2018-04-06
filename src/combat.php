cb17f18b8cd8c962fe31314f893844f285d0afa7
Tolrep <pierlotedouard@gmail.com>
2018-04-06 04:41:48 +0200

    SESSION pour  dans combat

--- src/combat.php
+++ src/combat.php
@@ -9,36 +9,36 @@ $round = 2;
 
 
 if (isset($_POST["hpHero1"])){
-    $hero[53]->hp = $_POST["hpHero1"];
+    $hero[($_SESSION['inputs']['customRadio1'])]->hp = $_POST["hpHero1"];
 }
 
 if (isset($_POST["hpHero2"])){
-    $hero[100]->hp = $_POST["hpHero2"];
+    $hero[($_SESSION['inputs']['customRadio2'])]->hp = $_POST["hpHero2"];
 }
 
 if (isset($_POST["submit1"])) {
    $round = 2;
-   ($hero[100])->atk($hero[53]);
-   $_SESSION['roundNumber'][] = ($hero[53]->hp);
-   //array_push($roundNumber,($hero[53]->hp));
+   ($hero[($_SESSION['inputs']['customRadio2'])])->atk($hero[($_SESSION['inputs']['customRadio1'])]);
+   $_SESSION['roundNumber'][] = ($hero[($_SESSION['inputs']['customRadio1'])]->hp);
+   //array_push($roundNumber,($hero[($_SESSION['inputs']['customRadio1'])]->hp));
 }
 
 if (isset($_POST["submit2"])) {
     $round = 1;
-    ($hero[53])->atk($hero[100]);
-    $_SESSION['roundNumber'][] = ($hero[100]->hp);
+    ($hero[($_SESSION['inputs']['customRadio1'])])->atk($hero[($_SESSION['inputs']['customRadio2'])]);
+    $_SESSION['roundNumber'][] = ($hero[($_SESSION['inputs']['customRadio2'])]->hp);
 
-    //array_push($roundNumber,($hero[100]->hp));
+    //array_push($roundNumber,($hero[($_SESSION['inputs']['customRadio2'])]->hp));
 }
 
-if ($hero[53]->hp === 0) {
-    echo $hero[100]->name . "a tue " . $hero[53]->name . " en combat singulier.";
+if ($hero[($_SESSION['inputs']['customRadio1'])]->hp === 0) {
+    echo $hero[($_SESSION['inputs']['customRadio2'])]->name . " a tue " . $hero[($_SESSION['inputs']['customRadio1'])]->name . " en combat singulier.";
     header("refresh:3;url=resume_du_combat.php");
     die;
 }
 
-if ($hero[100]->hp === 0) {
-    echo $hero[53]->name . "a tue " . $hero[100]->name . " en combat singulier.";
+if ($hero[($_SESSION['inputs']['customRadio2'])]->hp === 0) {
+    echo $hero[($_SESSION['inputs']['customRadio1'])]->name . " a tue " . $hero[($_SESSION['inputs']['customRadio2'])]->name . " en combat singulier.";
     header("refresh:3;url=resume_du_combat.php");
     die;
 }
@@ -51,30 +51,30 @@ if ($hero[100]->hp === 0) {
     </head>
     <body>
     <?php if ($round === 1){?>
-        <h1>Attaquant: <?php echo ($hero[100]->name)?></h1>
-        <h1>Defenseur: <?php echo ($hero[53]->name)?></h1>
+        <h1>Attaquant: <?php echo ($hero[($_SESSION['inputs']['customRadio2'])]->name)?></h1>
+        <h1>Defenseur: <?php echo ($hero[($_SESSION['inputs']['customRadio1'])]->name)?></h1>
         <form action="" method="post">
-            <input type="hidden" name="hpHero1" value="<?=$hero[53]->hp?>">
-            <input type="hidden" name="hpHero2" value="<?=$hero[100]->hp?>">
+            <input type="hidden" name="hpHero1" value="<?=$hero[($_SESSION['inputs']['customRadio1'])]->hp?>">
+            <input type="hidden" name="hpHero2" value="<?=$hero[($_SESSION['inputs']['customRadio2'])]->hp?>">
             <input type="submit" value="Attaque" name="submit1">
         </form>
     <?php }else {?>
-        <h1>Attaquant: <?php echo ($hero[53]->name)?></h1>
-        <h1>Defenseur: <?php echo ($hero[100]->name)?></h1>
+        <h1>Attaquant: <?php echo ($hero[($_SESSION['inputs']['customRadio1'])]->name)?></h1>
+        <h1>Defenseur: <?php echo ($hero[($_SESSION['inputs']['customRadio2'])]->name)?></h1>
         <form action="" method="post">
-            <input type="hidden" name="hpHero1" value="<?=$hero[53]->hp?>">
-            <input type="hidden" name="hpHero2" value="<?=$hero[100]->hp?>">
+            <input type="hidden" name="hpHero1" value="<?=$hero[($_SESSION['inputs']['customRadio1'])]->hp?>">
+            <input type="hidden" name="hpHero2" value="<?=$hero[($_SESSION['inputs']['customRadio2'])]->hp?>">
             <input type="submit" value="Attaque" name="submit2">
         </form>
     <?php }?>
     </body>
 </html>
 <?php
-echo $hero[100] . '<br>';
-echo $hero[53] . '<br>';
+
+echo $hero[($_SESSION['inputs']['customRadio1'])] . '<br>';
+echo $hero[($_SESSION['inputs']['customRadio2'])] . '<br>';
 
 
-print_r($_SESSION);
 
 ?>
 
