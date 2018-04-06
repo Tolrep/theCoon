edc44754fa38e03e0b7f1319b8477a4b336413db
Flavien Girier <girier.flavien@gmail.com>
2018-04-06 09:57:45 +0200

    modif form

--- src/Form.php
+++ src/Form.php
@@ -72,7 +72,7 @@ require 'creationSuperhero.php';
                         };?>
                         <div class="thumbnail">
                             <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio<?=$i?>" name="customRadio1" class="custom-control-input" <?php if ($_SESSION['inputs']['customRadio1']==1){echo ("checked");} ?> value="<?=($hero[$idHero])->id?>">
+                                <input type="radio" id="customRadio<?=$i?>" name="customRadio1" class="custom-control-input" value="<?=($hero[$idHero])->id?>">
                                 <label class="custom-control-label" for="customRadio<?=$i?>">Toggle this custom radio</label>
                             </div>
                             <p><?=$hero[$idHero]?></p>
@@ -94,7 +94,7 @@ require 'creationSuperhero.php';
                         <div class="thumbnail">
                             <div class="custom-control custom-radio">
                                 <label class="custom-control-label" for="customRadio<?=$l?>">Toggle this custom radio</label>
-                                <input type="radio" id="customRadio<?=$l?>" name="customRadio2" class="custom-control-input" <?php if ($_SESSION['inputs']['customRadio1']==1){echo ("checked");} ?> value="<?=($hero[$idAd])->id?>">
+                                <input type="radio" id="customRadio<?=$l?>" name="customRadio2" class="custom-control-input" value="<?=($hero[$idAd])->id?>">
                                 <label class="custom-control-label" for="customRadio<?=$l?>">Toggle this custom radio</label>
                             </div>
                             <p><?=$hero[$idAd]?></p>
@@ -121,7 +121,6 @@ require 'creationSuperhero.php';
 
 
 <?php
-var_dump(   $_SESSION['inputs']);
 unset($_SESSION['inputs']);
 unset($_SESSION['errors']);
 unset($_SESSION['success']);
